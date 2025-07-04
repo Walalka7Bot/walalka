818acf8ed385df4e41bfc7fc02563d8b389dd571
Walalka7Bot <husseinmohamedm88@gmail.com>
2025-07-04 17:39:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 import os
 from dotenv import load_dotenv
-
-# Load .env
 load_dotenv()
 
-# Sirtaada
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-INFURA_URL = os.getenv("INFURA_URL")  # optional, blockchain support
