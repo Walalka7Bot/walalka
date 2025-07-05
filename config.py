4a1b890d87dbfbd28a4bfb1d26a144acafe5dcc3
Walalka7Bot <husseinmohamedm88@gmail.com>
2025-07-05 14:46:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
+# config.py
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+BOT_TOKEN = "7646107517:AAG19U9yHKE1St1bTC86AbFweslqZOack6c"  # Tusaale ahaan
+CHAT_ID =6846894934  # Bedel tan adiga chat ID-gaaga sax ah
